dac548fdc56a4b92764eef4b4fa3b41dd22aed92
TendTo <casablancaernesto@gmail.com>
2021-07-06 23:16:28 +0200

    refactor: folder setup

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -3,7 +3,6 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render( <App />);
+  expect(screen.getByText(/Whale watch/i)).toBeInTheDocument();
 });
---
+++ src/components/App/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import DockerImage from '../DockerImage/DockerImage'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Whale watch
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
