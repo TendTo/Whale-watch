affa71d25361f4c49470fa6dcc514a795d13066d
TendTo <casablancaernesto@gmail.com>
2021-07-08 15:42:05 +0200

    feat: improved accessibility

--- src/components/DockerRemoteForm/DockerRemoteForm.tsx
+++ src/components/DockerRemoteForm/DockerRemoteForm.tsx
@@ -1,36 +1,29 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Row from "react-bootstrap/Row";
+import Tooltip from "react-bootstrap/Tooltip";
 import { useForm } from "react-hook-form";
-import { DockerRemoteData } from '../../types/DockerTypes';
 import { DockerRemoteContext } from '../../context/DockerRemoteContext';
-import { useContext } from "react";
+import { DockerRemoteData } from '../../types/DockerTypes';
+import './DockerRemoteForm.css';
+import strings from './DockerRemoteForm.json';
 
 function DockerRemoteForm() {
-    const placeholderCa = `-----BEGIN CERTIFICATE-----
-    MIIGGTCCBAGgAwIBAgIdasdasdggbZEprmjKoWclqyd+zmQwDQYJKoZIhvcNAQEL
-    BQAwgZsxCzAJBgNVBAA56dGBaT4wDAYDVQQIDAVJdGFseTERMA8GA1UEBwwIUy5U
-    -----END CERTIFICATE-----`;
-    const placeholderCert = `-----BEGIN CERTIFICATE-----
-    04dNMzgKIRZWt/WJucI7uPnd90g3XaD2z9LTfpQbs5ft5+qMyAiwMgvvFKOfK3gY
-    R8V6seVvA9BCJOEnTcxcl8dGeFbNtQLi2qkzk+CqTKwhIFjRatmQl00qBdnnR/4u
-    -----END CERTIFICATE-----`;
-    const placeholderKey = `-----BEGIN RSA PRIVATE KEY-----
-    2bMtAoIBAHV+68HOkCKGe4ybfYGYA6eGyro6udeR5Qf8cRyUdLK1W0BhWUoHrRpb
-    xGIK3tCf3iXk8vtdm514snHGvZkh/kdvHtlWmA8QR0XeDoYh17egWZB+6OrxEV6m
-    -----END RSA PRIVATE KEY-----`;
-    const placeholderHost = "hostname.com";
-    const placeholderPort = "2375";
-    const placeholderSocket = "/var/run/docker.sock"
     const defaultData = {
         socket: "/var/run/docker.sock", port: 2375, host: "192.168.1.18",
         ca: "",
         cert: "",
         key: ""
     }
+    const infoTooltip = (
+        <Tooltip id="infoTooltip">
+            {strings.infoTooltip}
+        </Tooltip>
+    )
 
     const [show, setShow] = useState(false);
     const [local, setLocal] = useState(false);
@@ -57,43 +50,53 @@ function DockerRemoteForm() {
                     <Modal.Title>Add a new Docker remote</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
+
                     <Form id="dockerRemoteForm" onSubmit={handleSubmit(onSubmit)}>
-                        <Form.Group controlId="dockerRemoteForm.local">
-                            <Form.Check
-                                disabled
-                                type="switch"
-                                id="dockerRemoteForm.local"
-                                label={local ? "Local Docker" : "Remote Docker"}
-                                onChange={() => setLocal(!local)}
-                            />
-                        </Form.Group>
+                        <Row>
+                            <Col xs="11">
+                                <Form.Group controlId="dockerRemoteForm.local">
+                                    <Form.Check
+                                        disabled
+                                        type="switch"
+                                        id="dockerRemoteForm.local"
+                                        label={local ? "Local Docker" : "Remote Docker"}
+                                        onChange={() => setLocal(!local)}
+                                    />
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <OverlayTrigger placement="right" delay={{ show: 150, hide: 300 }} overlay={infoTooltip}>
+                                    <i className="fa fa-2x fa-question-circle"></i>
+                                </OverlayTrigger>
+                            </Col>
+                        </Row>
                         {!local && (
                             <>
                                 <Row>
                                     <Col xs="6">
                                         <Form.Group controlId="dockerRemoteForm.host">
-                                            <Form.Label>Remote hostname or IP address</Form.Label>
-                                            <Form.Control {...register("host")} type="text" placeholder={placeholderHost} required={!local} />
+                                            <Form.Label className="DockerRemoteForm-required-label">Remote hostname or IP address</Form.Label>
+                                            <Form.Control {...register("host")} type="text" placeholder={strings.placeholderHost} required={!local} />
                                         </Form.Group>
                                     </Col>
                                     <Col xs="3">
                                         <Form.Group controlId="dockerRemoteForm.host">
-                                            <Form.Label>Remote port</Form.Label>
-                                            <Form.Control {...register("port", { valueAsNumber: true })} type="number" placeholder={placeholderPort} required={!local} />
+                                            <Form.Label className="DockerRemoteForm-required-label">Remote port</Form.Label>
+                                            <Form.Control {...register("port", { valueAsNumber: true })} type="number" placeholder={strings.placeholderPort} required={!local} />
                                         </Form.Group>
                                     </Col>
                                 </Row>
                                 <Form.Group controlId="dockerRemoteForm.ca">
                                     <Form.Label>Certificate Authority (ca.pem)</Form.Label>
-                                    <Form.Control {...register("ca")} as="textarea" rows={4} placeholder={placeholderCa} />
+                                    <Form.Control {...register("ca")} as="textarea" rows={4} placeholder={strings.placeholderCa} />
                                 </Form.Group>
                                 <Form.Group controlId="dockerRemoteForm.cert">
                                     <Form.Label>Certificate (cert.pem)</Form.Label>
-                                    <Form.Control {...register("cert")} as="textarea" rows={4} placeholder={placeholderCert} />
+                                    <Form.Control {...register("cert")} as="textarea" rows={4} placeholder={strings.placeholderCert} />
                                 </Form.Group>
                                 <Form.Group controlId="dockerRemoteForm.key">
                                     <Form.Label>Key (key.pem)</Form.Label>
-                                    <Form.Control {...register("key")} as="textarea" rows={4} placeholder={placeholderKey} />
+                                    <Form.Control {...register("key")} as="textarea" rows={4} placeholder={strings.placeholderKey} />
                                 </Form.Group>
                             </>
                         )}
@@ -101,7 +104,7 @@ function DockerRemoteForm() {
                             <>
                                 <Form.Group controlId="dockerRemoteForm.socket">
                                     <Form.Label>Socket path</Form.Label>
-                                    <Form.Control {...register("socket")} type="text" placeholder={placeholderSocket} required={local} />
+                                    <Form.Control {...register("socket")} type="text" placeholder={strings.placeholderSocket} required={local} />
                                 </Form.Group>
                             </>
                         )}
@@ -115,7 +118,7 @@ function DockerRemoteForm() {
                         Add
                     </Button>
                 </Modal.Footer>
-            </Modal>
+            </Modal >
         </>
     );
 }
