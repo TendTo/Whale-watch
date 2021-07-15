f7e1adfa50fc182782c896912f56b0c38936f638
TendTo <casablancaernesto@gmail.com>
2021-07-15 13:24:19 +0200

    style: minor style changes

--- src/components/MainNavbar/MainNavbar.tsx
+++ src/components/MainNavbar/MainNavbar.tsx
@@ -4,6 +4,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from 'react-router-dom';
 import DockerRemoteForm from "../DockerRemoteForm/DockerRemoteForm";
+import './MainNav.css';
 
 function MainNavbar() {
     return (
